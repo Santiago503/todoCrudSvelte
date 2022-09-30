11c141db0d673715d670baa38a1ae2953493b69d
santiago Encarnacion Smith <santiago.503@hotmail.com>
2022-09-30 17:12:29 -0400

    add storage for save

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,12 @@
   let todos = [];
   let todo = { id: "", text: "", estado: false };
 
+  const nameStorage = "todos";
+
+  if (localStorage.getItem("todos")) {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
   const addTodo = () => {
     if (!todo.text) {
       return;
@@ -9,12 +15,15 @@
 
     todo.id = Date.now();
     todos = [...todos, todo];
-    console.log(todo);
     todo = { id: "", text: "", estado: false };
   };
+
+  $: localStorage.setItem(nameStorage, JSON.stringify(todos));
+
   const delTodo = (id) => {
     todos = todos.filter((item) => item.id !== id);
   };
+
   const editTodo = (id) => {
     todos = todos.map((item) => {
       if (item.id === id) {
@@ -23,17 +32,21 @@
           estado: !item.estado,
         };
       }
-
       return {
-        item,
+        ...item,
       };
     });
+
   };
+
+  $: classIcon = (valor) => (valor ? "bi bi-arrow-clockwise" : "bi bi-check2");
+
+  const classEstado = (valor) => (valor ? "btn-success" : "btn-warning");
 </script>
 
 <div class="container">
-  <h1 class="display-5 my-3">
-    <h1>Hola Mundo</h1>
+  <h1 class="display-5 my-3 text-center">
+    <h1>Hola Mundo Todo en Svelte</h1>
   </h1>
 
   <form on:submit|preventDefault={addTodo}>
@@ -47,11 +60,20 @@
 
   {#each todos as td}
     <div class="shadow my-3 p-3 lead d-flex justify-content-between">
-      <p>{td.text}</p>
+      <p
+        class={(td.estado
+          ? "text-decoration-line-through"
+          : "") + " text-capitalize"}
+      >
+        {td.text}
+      </p>
       <div>
-        <button class="btn btn-sm btn-warning" on:click={editTodo(td.id)}
-          ><i class="bi bi-pen" /></button
+        <button
+          class="btn btn-sm {classEstado(td.estado)}"
+          on:click={editTodo(td.id)}
         >
+          <i class={classIcon(td.estado)} />
+        </button>
         <button class="btn btn-sm btn-danger" on:click={delTodo(td.id)}
           ><i class="bi bi-trash" /></button
         >
